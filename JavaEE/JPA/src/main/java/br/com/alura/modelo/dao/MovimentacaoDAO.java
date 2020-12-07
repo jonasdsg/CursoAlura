756e886f6f3196571a3b2a524f33a446460fe8d1
Jonas Góes <jonasdossantos1991@gmail.com>
2020-12-07 11:59:28 -0300

    Reajuste do DAO - Uso de injeção

--- JavaEE/JPA/src/main/java/br/com/alura/modelo/dao/MovimentacaoDAO.java
+++ JavaEE/JPA/src/main/java/br/com/alura/modelo/dao/MovimentacaoDAO.java
@@ -4,32 +4,35 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 import br.com.alura.modelo.Conta;
 import br.com.alura.modelo.Movimentacao;
 
 public class MovimentacaoDAO {
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-	EntityManager em = emf.createEntityManager();
-	String jpql;
-	public List<Movimentacao> getMovimentacoesConta(Conta conta){
+	private EntityManager em;
+	private String jpql;
+
+	public MovimentacaoDAO(EntityManagerFactory emf) {
+		em = emf.createEntityManager();
+	}
+
+	public List<Movimentacao> getMovimentacoesConta(Conta conta) {
 		jpql = "select c from Conta c left join fetch c.movimentacoes where :pconta = c";
-		TypedQuery<Conta> query = em.createQuery(jpql,Conta.class);
-		query.setParameter("pconta",conta);
+		TypedQuery<Conta> query = em.createQuery(jpql, Conta.class);
+		query.setParameter("pconta", conta);
 		return query.getSingleResult().getMovimentacoes();
 	}
-	
+
 	public Double getMediaMovimentacoes() {
 		jpql = "select sum(m.valor) from Movimentacao m";
-		TypedQuery<Double> avg = em.createQuery(jpql,Double.class);
+		TypedQuery<Double> avg = em.createQuery(jpql, Double.class);
 		return avg.getSingleResult();
 	}
-	
+
 	public List<MovimentacaoAgrupadoPorData> getSomaDasMovimentacoesAgrupadasPorData() {
 		jpql = "select new br.com.alura.modelo.dao.MovimentacaoAgrupadoPorData(sum(m.valor),day(m.data),month(m.data)) from Movimentacao m group by day(m.data)";
-		TypedQuery<MovimentacaoAgrupadoPorData> group = em.createQuery(jpql,MovimentacaoAgrupadoPorData.class);
+		TypedQuery<MovimentacaoAgrupadoPorData> group = em.createQuery(jpql, MovimentacaoAgrupadoPorData.class);
 		return group.getResultList();
 	}
 }
--- JavaEE/JPA/src/main/java/br/com/alura/testes/TestaMovimentacaoDao.java
+++ JavaEE/JPA/src/main/java/br/com/alura/testes/TestaMovimentacaoDao.java
@@ -1,5 +1,7 @@
 package br.com.alura.testes;
 
+import javax.persistence.Persistence;
+
 import br.com.alura.modelo.Conta;
 import br.com.alura.modelo.dao.MovimentacaoDAO;
 
@@ -8,7 +10,7 @@ public class TestaMovimentacaoDao {
 	public static void main(String[] args) {
 		Conta c = new Conta();
 		c.setId(1L);
-		MovimentacaoDAO dao = new MovimentacaoDAO();
+		MovimentacaoDAO dao = new MovimentacaoDAO(Persistence.createEntityManagerFactory("contas"));
 		
 		dao.getMovimentacoesConta(c).forEach(m->{
 			System.out.println(m);
@@ -17,6 +19,7 @@ public class TestaMovimentacaoDao {
 		dao.getSomaDasMovimentacoesAgrupadasPorData().forEach(m->{
 			System.out.println("Dia: "+m.getDia()+'/'+m.getMes());
 			System.out.println("Valor: "+m.getValor());
+			System.out.println("___________________________");
 		});
 	}
 
