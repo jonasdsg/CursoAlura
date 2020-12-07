81f43597674a2239e5a7f082da209af0928ec88a
Jonas Góes <jonasdossantos1991@gmail.com>
2020-12-07 10:57:20 -0300

    Uso da função group by

--- JavaEE/JPA/src/main/java/br/com/alura/testes/TestandoSomaDasMovimentacoes.java
+++ JavaEE/JPA/src/main/java/br/com/alura/testes/TestandoSomaDasMovimentacoes.java
@@ -14,8 +14,21 @@ public class TestandoSomaDasMovimentacoes {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
 		EntityManager em = emf.createEntityManager();
 		String jpql = "select m.valor from Movimentacao m";
+
+		//Coletando os valores de uma tabela
 		TypedQuery<Double> query = em.createQuery(jpql,Double.class);
 		List<Double> lista = query.getResultList();
 		lista.forEach(m -> System.out.println(m));
+		
+		//Obterndo a media
+		jpql = "select sum(m.valor) from Movimentacao m";
+		TypedQuery<Double> avg = em.createQuery(jpql,Double.class);
+		System.out.println(avg.getSingleResult());
+		
+		//Group by
+		jpql = "select m.valor from Movimentacao m group by day(m.data)";
+		TypedQuery<Double> group = em.createQuery(jpql,Double.class);
+		System.out.println(group.getResultList());
+		
 	}
 }
