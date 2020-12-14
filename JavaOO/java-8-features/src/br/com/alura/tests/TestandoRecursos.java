e1a180b53e4a31313e8dd4621b5c531f5bfd38ff
Jonas Góes <jonasdossantos1991@gmail.com>
2020-12-14 11:34:13 -0300

    Treinando as features do java 8

---
+++ JavaOO/java-8-features/src/br/com/alura/tests/TestandoRecursos.java
@@ -0,0 +1,25 @@
+package br.com.alura.tests;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class TestandoRecursos {
+
+	public static void main(String[] args) {
+		List<String> array = Arrays.asList("Jonas","Carlos","Amanda","Roberta","Bruna");
+		Consumer<String> iterador = new Iterador();
+		array.forEach(iterador);
+		
+	}
+
+}
+
+class Iterador implements Consumer<String>{
+
+	@Override
+	public void accept(String nome) {
+		System.out.println(nome);
+	}
+	
+}
