f38600cfc8644411ad94736ac1abce58b9f951ff
Jonas Góes <jonasdossantos1991@gmail.com>
2020-12-14 11:53:08 -0300

    Ordenando elementos com o sort utilizando classe anonima com lambda expression.

--- JavaOO/java-8-features/src/br/com/alura/tests/TestandoRecursos.java
+++ JavaOO/java-8-features/src/br/com/alura/tests/TestandoRecursos.java
@@ -8,8 +8,10 @@ public class TestandoRecursos {
 
 	public static void main(String[] args) {
 		List<String> array = Arrays.asList("Jonas","Carlos","Amanda","Roberta","Bruna");
-		
+		System.out.println("Ordem de inserção :\n");
 		array.forEach(nome -> System.out.println(nome));
-
+		array.sort((nome1,nome2)-> nome1.compareTo(nome2));
+		System.out.println("\nOrdem alfabética: \n");
+		array.forEach(nome -> System.out.println(nome));
 	}
 }
