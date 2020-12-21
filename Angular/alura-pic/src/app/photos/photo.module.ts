6abcac4309d99a19496c5eaa3eb5beb25506ce30
Jonas Góes <jonasdossantos1991@gmail.com>
2020-12-21 13:20:33 -0300

    Usando rotas

--- Angular/alura-pic/src/app/photos/photo.module.ts
+++ Angular/alura-pic/src/app/photos/photo.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
     declarations: [
         PhotoComponent,
-        PhotoListComponent
+        PhotoListComponent,
+        PhotoFormComponent
     ],
 
     exports: [
