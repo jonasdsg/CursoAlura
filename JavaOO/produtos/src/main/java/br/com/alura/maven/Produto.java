264123337542fbdb5e8b4f68d5887e2929993557
Jonas Góes <jonasdossantos1991@gmail.com>
2020-12-22 13:51:06 -0300

    Updates

---
+++ JavaOO/produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -0,0 +1,24 @@
+package br.com.alura.maven;
+
+public class Produto {
+	private String nome;
+	private Double valor;
+	public Produto(String nome, Double valor) {
+		super();
+		this.nome = nome;
+		this.valor = valor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+}
