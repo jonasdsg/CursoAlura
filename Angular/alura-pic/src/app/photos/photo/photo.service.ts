da3bfd1c7d616fbe438268759413569ade443c9c
Jonas Góes <jonasdossantos1991@gmail.com>
2021-01-19 09:23:43 -0300

    Angular - pegando parametros do navegador e realizando paginação na listagem de fotos.

--- Angular/alura-pic/src/app/photos/photo/photo.service.ts
+++ Angular/alura-pic/src/app/photos/photo/photo.service.ts
@@ -1,5 +1,5 @@
 import { Photo } from './photo';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 const URL:string = 'http://localhost:3000';
@@ -14,4 +14,9 @@ export class PhotoService{
     listFromUser(userName:string):Observable<Photo[]>{
        return this.http.get<Photo[]>(`${URL}/${userName}/photos`);
     }
+
+    listFromUserPaginated(userName:string,page:number):Observable<Photo[]>{
+        const params = new HttpParams().append('page',page.toString());
+        return this.http.get<Photo[]>(`${URL}/${userName}/photos`,{params});
+     }
 }
