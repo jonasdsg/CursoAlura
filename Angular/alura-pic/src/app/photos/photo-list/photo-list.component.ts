8610ecdb661730e63f6b3492012da7da2567c4f4
Jonas Góes <jonasdossantos1991@gmail.com>
2021-01-19 09:13:33 -0300

    Angular - Adicionando o padrão debounce para evitar muitas requisições ao servidor.

--- Angular/alura-pic/src/app/photos/photo-list/photo-list.component.ts
+++ Angular/alura-pic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,22 +1,35 @@
 import { PhotoService } from './../photo/photo.service';
 import { Photo } from './../photo/photo';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   public photos:Photo[] = [];
+  public debounce:Subject<string> = new Subject<string>();
+  public filtro:string = '';
+
   constructor(
     private activatedRoute:ActivatedRoute
     ){}
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
   
   ngOnInit(): void {
    this.photos = this.activatedRoute.snapshot.data['photos'];
+   this.debounce
+   .pipe(debounceTime(300))
+   .subscribe(filtro => this.filtro = filtro)
+   ;
   }
 
 }
