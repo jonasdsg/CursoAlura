bad3cbd1004f3ef7cbbbe90e218da942003d3e3f
Jonas Góes <jonasdossantos1991@gmail.com>
2021-01-25 22:17:04 -0300

    configurando build em prod e variáveis de ambiente

--- WebPack/projeto-webpack/client/webpack.config.js
+++ WebPack/projeto-webpack/client/webpack.config.js
@@ -1,11 +1,16 @@
-const path = require('path')
+const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+let plugins = [];
+if(process.env.NODE_ENV == 'production'){
+    plugins.push(new babiliPlugin());
+}
 module.exports = {
     entry: './app-src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname,'dist')
     },
-    
+
     module:{
 
         rules: [
@@ -17,5 +22,6 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins
 }
