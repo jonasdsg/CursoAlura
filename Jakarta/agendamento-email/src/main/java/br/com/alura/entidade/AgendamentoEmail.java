409a4fdb81c56f2bff3313d6c92626a20f5375a2
Jonas Góes <jonasdossantos1991@gmail.com>
2021-04-23 18:26:55 -0300

    usando mensagens com jms

--- Jakarta/agendamento-email/src/main/java/br/com/alura/entidade/AgendamentoEmail.java
+++ Jakarta/agendamento-email/src/main/java/br/com/alura/entidade/AgendamentoEmail.java
@@ -2,12 +2,16 @@ package br.com.alura.entidade;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class AgendamentoEmail {
+public class AgendamentoEmail implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	private Long id;
--- Jakarta/agendamento-email/src/main/java/br/com/alura/jobs/AgendamentoEmailJob.java
+++ Jakarta/agendamento-email/src/main/java/br/com/alura/jobs/AgendamentoEmailJob.java
@@ -1,8 +1,12 @@
 package br.com.alura.jobs;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import br.com.alura.servicos.EmailServico;
 
@@ -11,11 +15,19 @@ public class AgendamentoEmailJob {
 	@Inject
 	private EmailServico emailService;
 	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+	private JMSContext context;
+	
+	@Resource(mappedName = "java:/jms/queue/EmailQueue")
+	private Queue queue;
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmails() {
 		
 		emailService.listarPorNaoAgendados().forEach(email->{
-			emailService.enviar(email);
+			context.createProducer().send(queue, email);
+			emailService.alterar(email);
 		});
 	}
 	
